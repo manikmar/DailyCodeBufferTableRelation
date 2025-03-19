05c4e066e2a04ca19d0a2828b605375db65e88f9
Mani <manikumar.panithi@gmail.com>
2025-03-18 20:25:55 -0400

    add project to git

---
+++ src/main/java/com/fm/rel/DailyCodeBufferTableRelation/DailyCodeBufferTableRelationApplication.java
@@ -0,0 +1,13 @@
+package com.fm.rel.DailyCodeBufferTableRelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCodeBufferTableRelationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCodeBufferTableRelationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fm/rel/DailyCodeBufferTableRelation/DailyCodeBufferTableRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.rel.DailyCodeBufferTableRelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCodeBufferTableRelationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
