4d8093d88adbebe0e80cee57b53260298a92ced9
Mani <manikumar.panithi@gmail.com>
2025-03-22 15:47:59 -0400

    add logs

--- src/main/java/com/fm/rel/DailyCodeBufferTableRelation/controller/StudentController.java
+++ src/main/java/com/fm/rel/DailyCodeBufferTableRelation/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.fm.rel.DailyCodeBufferTableRelation.controller;
 
 import com.fm.rel.DailyCodeBufferTableRelation.entity.Student;
 import com.fm.rel.DailyCodeBufferTableRelation.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
     @Autowired
     private StudentService studentService;
 
     @PostMapping("/saveStudent")
     public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        logger.info("saveStudent");
         Student studentData = studentService.saveStudent(student);
         return ResponseEntity.status(HttpStatus.CREATED).body(studentData);
     }
--- src/main/java/com/fm/rel/DailyCodeBufferTableRelation/service/StudentService.java
+++ src/main/java/com/fm/rel/DailyCodeBufferTableRelation/service/StudentService.java
@@ -2,16 +2,20 @@ package com.fm.rel.DailyCodeBufferTableRelation.service;
 
 import com.fm.rel.DailyCodeBufferTableRelation.entity.Student;
 import com.fm.rel.DailyCodeBufferTableRelation.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     @Autowired
     private StudentRepository studentRepository;
 
     public Student saveStudent(Student student){
+        logger.info("saveStudent service");
         return studentRepository.save(student);
     }
 }
